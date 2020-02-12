ce2977107e8a0b00c3a46b7d49522b5a4588664e
Ethan Nelson <36721383+einelson8i@users.noreply.github.com>
2020-02-12 15:21:52 -0700

    no scripts

--- ScriptSheet.js
+++
@@ -1,8 +0,0 @@
-function nav_change() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav";
-  }
-}
