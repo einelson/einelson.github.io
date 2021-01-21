bab7f73bd4db27213932679ea513dac64630c090
Ethan Nelson <36721383+einelson8i@users.noreply.github.com>
2021-01-20 17:09:11 -0700

    new everything

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
-/**
- * Dropdown menu
- */
+// Burger menus
 document.addEventListener('DOMContentLoaded', function() {
-    const menus = document.querySelectorAll('.navbar-burger');
-    const dropdowns = document.querySelectorAll('.navbar-menu');
+    // open
+    const burger = document.querySelectorAll('.navbar-burger');
+    const menu = document.querySelectorAll('.navbar-menu');
 
-    if (menus.length && dropdowns.length) {
-        for (var i = 0; i < menus.length; i++) {
-            menus[i].addEventListener('click', function() {
-                for (var j = 0; j < dropdowns.length; j++) {
-                    dropdowns[j].classList.toggle('hidden');
+    if (burger.length && menu.length) {
+        for (var i = 0; i < burger.length; i++) {
+            burger[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    // close
+    const close = document.querySelectorAll('.navbar-close');
+    const backdrop = document.querySelectorAll('.navbar-backdrop');
+
+    if (close.length) {
+        for (var i = 0; i < close.length; i++) {
+            close[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    if (backdrop.length) {
+        for (var i = 0; i < backdrop.length; i++) {
+            backdrop[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
                 }
             });
         }
